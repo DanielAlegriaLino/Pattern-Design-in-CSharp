884a03cf9e58366718a8f09daeaae181f5fa652e
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-11-29 13:31:47 -0600

    First program status

---
+++ csharp_unit_testing_unix_1/Comportamientos/Comportamiento_Quack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_unit_testing_unix_1
+{
+    internal interface Comportamiento_Quack
+    {
+        public void Respirar();
+        public void Quack();
+    }
+}
---
+++ csharp_unit_testing_unix_1/Quack_Feroz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace csharp_unit_testing_unix_1.Comportamientos
+{
+    internal class Quack_Feroz : Comportamiento_Quack
+    {
+        public void Quack() 
+        {
+            WriteLine("Hacer un quakido de macho alfa");
+        }
+        
+        public void Respirar() 
+        {
+            WriteLine();
+        }
+
+
+    }
+}
