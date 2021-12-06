19bc72d546f3ef408cd4cda28753415f20f361ba
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-06 00:14:57 -0600

    Implementado el patron Observer en un ejemplo de youtube

---
+++ csharp_pattern_design_observer/Codigo/ShowLastNotifications/ObservePattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Codigo.ShowLastNotifications;
+
+public abstract class ISujeto
+{
+	public List<IObserver> observers = new List<IObserver> { };
+
+	public abstract void AddObserver(IObserver a_observer);
+	public abstract void RemoveObserver(IObserver a_observer);
+	public abstract void NotifyObservers();
+}
+
+public interface IObserver
+{
+	public void Update(string a_last_title, string a_last_url, string a_description, string a_last_image);
+}
+
+public interface IDisplay 
+{
+	public void DisplayInfo();
+}
+
+
