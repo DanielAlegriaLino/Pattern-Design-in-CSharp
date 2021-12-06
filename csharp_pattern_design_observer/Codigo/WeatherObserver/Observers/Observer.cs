73e72a1ee1c306f3d44109b4bc62c20c71a5c5d8
ObamaBolsa <daniel.alegria.lino@gmail.com>
2021-12-05 20:07:50 -0600

    Base Structure for start working created

---
+++ csharp_pattern_design_observer/Codigo/WeatherObserver/Observers/Observer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_pattern_design_observer.Codigo.WeatherObserver.Observers
+{
+	internal interface Observer
+	{
+
+	}
+}
